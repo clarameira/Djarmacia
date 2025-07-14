572ba645c6628db74bb0fdb7ccfec812a603b51a
clarameira <claraa.meiraa@hotmail.com>
2025-07-14 15:13:00 -0300

    alterações antes de iniciar git flow

--- farmacia/api/urls.py
+++ farmacia/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProdutoViewSet
+from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
+from core.views import TaskViewSet, NotificationAPIView
 
 router = DefaultRouter()
 router.register(r'produtos', ProdutoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+
+    path('api/notification/', NotificationAPIView.as_view(), name='notification'),
+
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
