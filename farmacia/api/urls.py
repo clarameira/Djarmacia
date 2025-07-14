0f105812c9449bac3ed3cf6201c9fffacce1c466
deborabckp <debora.costa22396@alunos.ufersa.edu.br>
2025-07-14 14:37:30 -0300

    pre commit update

--- farmacia/api/urls.py
+++ farmacia/api/urls.py
@@ -1,6 +1,8 @@
+"""URLs do app farmacia."""
+# pylint: disable=import-error
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ProdutoViewSet
+from rest_framework.routers import DefaultRouter # pylint: disable=no-member
+from farmacia.api.views import ProdutoViewSet # pylint: disable=no-member
 
 router = DefaultRouter()
 router.register(r'produtos', ProdutoViewSet)
--- farmacia/api/views.py
+++ farmacia/api/views.py
@@ -1,14 +1,11 @@
 """Views do app farmacia."""
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from farmacia.api.models import Produto  # pylint: disable=import-error
+from farmacia.api.serializers import ProdutoSerializer  # pylint: disable=import-error
 
 
-from farmacia.api.models import Produto
-from farmacia.api.serializers import ProdutoSerializer  
-
-
-class ProdutoViewSet(viewsets.ModelViewSet):
+class ProdutoViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """ViewSet para manipular produtos da farmácia."""
 
     queryset = Produto.objects.all()
